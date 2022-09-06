e1a044c806e7951dc451dd890e53f0c7d84d3935
testinprod <testingprod8080@gmail.com>
2022-09-06 18:46:09 -0500

    join connected to contract

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-  import { signerAddress, defaultEvmStores } from 'svelte-ethers-store';
+	import { connected, contracts, defaultEvmStores } from "svelte-ethers-store";
 	import GameManager from '../files/GameManager.json';
-
-	onMount(
-  	async () => {
-      // add a test to return in SSR context
-  		// defaultEvmStores.attachContract('imposter', CONTRACT_ADDRESS, GameManager.abi)
-      
-			promise = defaultEvmStores.setProvider();
-    }
-  )
-
-	let promise;
 
 	let games = [
 		{
+			name: "joes-game",
 			address: '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512',
 			joined: 2,
 			max: 10
 		}
 	]
 
-  // $: account = $connected && $signer ? $signer.getAddress() : ''
+	async function getGameContract(contractAddress: string) {
+		defaultEvmStores.attachContract(contractAddress, contractAddress, JSON.stringify(GameManager.abi));
+
+		const count = await $contracts[contractAddress].getPlayerCount();
+
+		return {
+			count: count
+		};
+	}
 </script>
 
 <svelte:head>
@@ -30,35 +27,38 @@
 	<meta name="description" content="Join game page" />
 </svelte:head>
 
-<div class="w-full">
-	<span class="items-end">{$signerAddress}</span>
-</div>
 <section class="flex flex-1 flex-col justify-center items-center space-y-5">
-	<h1>Join Game</h1>
-	<table class="table-auto">
-		<thead>
-			<tr>
-				<th>Address</th>
-				<th>Joined / Max</th>
-				<th></th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each games as game}
-				<tr>
-					<th>{game.address}</th>
-					<th>{game.joined} / {game.max}</th>
-					<th>
-						<a href={'/lobby/' + game.address}>
-							<button class="p-2 bg-slate-700 text-slate-200 rounded-md">
-								Join
-							</button>
-						</a>
-					</th>
+	{#if $connected}
+		<h1>Join Game</h1>
+		<table class="table-auto border border-slate-800">
+			<thead>
+				<tr class="border border-slate-800">
+					<th class="p-4 bg-slate-700">Address</th>
+					<th class="p-4 bg-slate-700">Joined / Max</th>
+					<th class="p-4 bg-slate-700"></th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
+			</thead>
+			<tbody class="border border-slate-800">
+				{#each games as game}
+					<!-- svelte-ignore empty-block -->
+					{#await getGameContract(game.address)}
+					{:then result}
+						<tr>
+							<th class="p-4 bg-slate-600">{game.address}</th>
+							<th class="p-4 bg-slate-600">{result.count} / {game.max}</th>
+							<th class="p-4 bg-slate-600">
+								<a href={'/lobby/' + game.address}>
+									<button class="p-2 bg-slate-700 text-slate-200 rounded-md">
+										Join
+									</button>
+								</a>
+							</th>
+						</tr>
+					{/await}
+				{/each}
+			</tbody>
+		</table>
+	{/if}
 </section>
 
 <style>
